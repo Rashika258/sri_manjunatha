a1adc30d78c713a04938c1fe7910766f8bc487df
Rashika258 <rashikasuresh5758@gmail.com>
2024-12-21 08:29:44 +0530

    --

--- components/common/app-filter.tsx
+++ components/common/app-filter.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import AppSearch from "./app-search";
 import { DateRange } from "react-day-picker";
--- components/common/app-form-header.tsx
+++ components/common/app-form-header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { Button } from "@/components/ui/index";
 import { ArrowLeftIcon } from "lucide-react";
