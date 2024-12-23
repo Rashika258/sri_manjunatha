4eabdc2c8bacf5fb7960ec8f952a8c9947bd7573
Rashika258 <rashikasuresh5758@gmail.com>
2024-12-23 17:22:41 +0530

    product route changes

--- app/api/product-category/[id]/route.ts
+++ app/api/product-category/[id]/route.ts
@@ -6,18 +6,20 @@ export async function DELETE(
   { params }: { params: { id: string } }
 ) {
   try {
-    const customerId = parseInt(params.id);
+    const categoryId = parseInt(params.id);
 
-    // Delete the customer
-    await prisma.customers.delete({
-      where: { customer_id: customerId },
+    const deletedProductCategory = await prisma.product_categories.delete({
+      where: { category_id: categoryId },
     });
 
-    return NextResponse.json({ message: "Customer deleted successfully" });
+    return NextResponse.json({
+      message: "Product Category deleted successfully",
+      data: deletedProductCategory,
+    });
   } catch (error) {
-    console.error("Error deleting customer:", error);
+    console.error("Error deleting product category:", error);
     return NextResponse.json(
-      { error: "Error deleting customer" },
+      { error: "Error deleting product category" },
       { status: 500 }
     );
   }
@@ -28,21 +30,19 @@ export async function PUT(
   { params }: { params: { id: string } }
 ) {
   try {
-    const customerId = parseInt(params.id);
+    const categoryId = parseInt(params.id);
     const data = await req.json();
 
-    // Validate or sanitize the data here as needed (e.g., check email format)
-
-    const updatedCustomer = await prisma.customers.update({
-      where: { customer_id: customerId },
+    const updatedCustomer = await prisma.product_categories.update({
+      where: { category_id: categoryId },
       data: data,
     });
 
     return NextResponse.json(updatedCustomer);
   } catch (error) {
-    console.error("Error updating customer:", error);
+    console.error("Error updating product category:", error);
     return NextResponse.json(
-      { error: "Error updating customer" },
+      { error: "Error updating product category" },
       { status: 500 }
     );
   }
@@ -50,23 +50,21 @@ export async function PUT(
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id?: string } } // id is now optional
+  { params }: { params: { id?: string } }
 ) {
   try {
     if (params.id) {
-      const customerId = parseInt(params.id);
+      const categoryId = parseInt(params.id);
 
-      // Validate if 'customerId' is a valid number
-      if (isNaN(customerId)) {
+      if (isNaN(categoryId)) {
         return NextResponse.json(
           { error: "Invalid customer ID" },
           { status: 400 }
         );
       }
 
-      // Fetch customer by customerId
-      const customer = await prisma.customers.findUnique({
-        where: { customer_id: customerId },
+      const customer = await prisma.product_categories.findUnique({
+        where: { category_id: categoryId },
       });
 
       if (!customer) {
@@ -79,10 +77,8 @@ export async function GET(
       return NextResponse.json(customer);
     }
 
-    // If no ID is provided, fetch all customers
     const customers = await prisma.customers.findMany();
     return NextResponse.json(customers);
-
   } catch (error) {
     console.error("Error fetching customer(s):", error);
     return NextResponse.json(
